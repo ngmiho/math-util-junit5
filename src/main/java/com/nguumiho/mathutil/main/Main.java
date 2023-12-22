f6fa8380b0992acad66def8ec5521e6bd5a5ab8f
nguumiho <minhhoang25498@gmail.com>
2023-12-22 11:46:26 +0700

    1st upload with Maven project

---
+++ src/main/java/com/nguumiho/mathutil/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.nguumiho.mathutil.main;
+
+import com.nguumiho.mathutil.core.MathUtil;
+
+/**
+ *
+ * @author TinCode
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("5! = " + MathUtil.getFactorial(5));
+        
+    }
+}
